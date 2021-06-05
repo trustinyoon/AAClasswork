c941a6b5d5530e87735e1b954c6603fbca7120e1
trustinyoon <trustinyoon@gmail.com>
2021-06-04 18:05:23 -0700

    tgif

--- W4D5/anagrams.rb
+++ W4D5/anagrams.rb
@@ -62,7 +62,48 @@ end
 #   hash1.values.all? {|val| val == 0}
 # end
 
-p first_anagram?("gizmo", "sally")    #=> false
-p first_anagram?("elvis", "lives")    #=> true
-p first_anagram?("ben", "neb")    #=> true
-p first_anagram?("trustin", "nitsurt")    #=> true
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
+# p first_anagram?("ben", "neb")    #=> true
+# p first_anagram?("trustin", "nitsurt")    #=> true
+
+def bad_two_sum?(arr, target_sum)
+  perm = arr.permutation(2).to_a
+  temp = []
+  perm.each do |sub|
+    if sub.sum == target_sum
+      temp << sub
+    end
+  end
+  !temp.empty?
+end
+
+
+# arr = [0, 1, 5, 7]
+# p bad_two_sum?(arr, 6) # => should be true
+# p bad_two_sum?(arr, 10) # => should be false
+# p bad_two_sum?(arr, 7)
+# p bad_two_sum?(arr, 3)
+
+def okay_two_sum?(arr, target_sum)
+  sorted = arr.sort
+  mid_idx = arr.length/2
+  return true if mid_idx == target_sum
+  return false if sorted.empty?
+
+  left = sorted[0...mid_idx]
+  right = sorted[mid_idx+1..-1]
+
+  if !sorted[mid_idx].nil? && target_sum > sorted[mid_idx]
+    okay_two_sum?(right, target_sum)
+  else
+    okay_two_sum?(left, target_sum)
+  end
+
+end
+
+ arr = [0, 1, 5, 7]
+ p okay_two_sum?(arr, 6) # => should be true
+ p okay_two_sum?(arr, 10) # => should be false
+ p okay_two_sum?(arr, 7)
+ p okay_two_sum?(arr, 3)
