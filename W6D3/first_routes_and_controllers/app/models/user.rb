269be030574451245ed13fdfb549928afba39825
nichathong <nicha.fn@gmail.com>
2021-06-17 11:54:13 -0700

    Added commas

--- W6D3/first_routes_and_controllers/app/models/user.rb
+++ W6D3/first_routes_and_controllers/app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   validates :username, presence: true
 
-    has_many :artworks, dependent: :destroy
+    has_many :artworks, dependent: :destroy,
       foreign_key: :artist_id,
       class_name: :ArtWork
 
-    has_many :artwork_shares, dependent: :destroy
+    has_many :artwork_shares, dependent: :destroy,
       foreign_key: :viewer_id,
       class_name: :ArtworkShare
 
