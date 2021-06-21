88bfe89e032c846445362b396328c5df6b360b12
canberko560 <canberkvarli@gmail.com>
2021-06-21 15:16:49 -0700

    add password methods

--- W7D1/skeleton/app/models/user.rb
+++ W7D1/skeleton/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :password_digest, presence: true
   after_initialize :ensure_session_token
 
+
   def ensure_session_token
     self.session_token ||= SecureRandom::urlsafe_base64 
   end
@@ -12,4 +13,26 @@ class User < ApplicationRecord
     self.save!
     self.session_token
   end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password) 
+  end
+
+
+  def is_password?(password) #VERIFIED?
+      self.password_digest == BCrypt::Password.create(password) #2a$10$175687215.87r16jfkahskjf14
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(params[:username])
+    if user && is_password?(password)
+        return user
+    else
+        return nil
+    end
+  end
+
+  
+
 end
