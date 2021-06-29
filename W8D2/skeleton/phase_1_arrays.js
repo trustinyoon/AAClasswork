e7e5564da8c4d60f04bcac0295ef9c298a882772
trustinyoon <trustinyoon@gmail.com>
2021-06-29 16:02:30 -0700

    new qs

--- W8D2/skeleton/phase_1_arrays.js
+++ W8D2/skeleton/phase_1_arrays.js
@@ -28,7 +28,25 @@ Array.prototype.twoSum = function () {
   return arr;
 };
 
-console.log([1, -2, 2, -3, 3, 3].twoSum());
-console.log([4, -4, 5, 6, -5].twoSum());
+// console.log([1, -2, 2, -3, 3, 3].twoSum());
+// console.log([4, -4, 5, 6, -5].twoSum());
 
 // ------------------------------------------------------------------------
+Array.prototype.transpose = function () {
+  let arr = [];
+
+  for (let i = 0; i < this.length; i++) {
+    arr.push([]); 
+  }
+// []
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 0; j < this.length; j++) {
+      // arr[j].push(this[i][j]);
+      arr[i][j] = this[j][i];
+    }
+  }
+
+  return arr;
+}
+
+console.log([[0, 1], [2, 3]].transpose()); // => [[0,2],[1,3]]
