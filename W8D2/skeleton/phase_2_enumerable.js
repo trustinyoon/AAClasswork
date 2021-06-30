d5f2b9bb3b8ad314b7380f7c6ac359700a7e731f
trustinyoon <trustinyoon@gmail.com>
2021-06-29 17:00:34 -0700

    myMap

--- W8D2/skeleton/phase_2_enumerable.js
+++ W8D2/skeleton/phase_2_enumerable.js
@@ -1,12 +1,24 @@
 Array.prototype.myEach= function (callback) {
-    this.forEach(ele => console.log(callback(ele)))
+    this.forEach(ele => callback(ele))
 } 
 
 function addOne(num) {
     return num + 1
 }
 
-console.log([1,2,3,4,5].myEach(addOne))
+// console.log([1,2,3,4,5].myEach(addOne))
+
+// ------------------------------------------------------------------------
+
+Array.prototype.myMap = function (callback) {
+  let arr = [];
+
+  this.myEach(el => arr.push(callback(el)))
+
+  return arr;
+}
+
+// console.log([1, 2, 3, 4, 5].myMap(addOne))
 
 // ------------------------------------------------------------------------
 
